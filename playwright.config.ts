50903a7e0b0bb09d68a3eba9d49c1054a742563d
RUSTEMATOR <samoilenkorustem@gmail.com>
2025-03-07 16:14:19 +0200

    a

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,12 +20,12 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 3,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
+  reporter: 'list',
+// 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: false,
